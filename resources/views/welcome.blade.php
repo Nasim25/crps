382ecae2041eb941fb98136f0fddc29ab60f71b2
Nasim25 <nasimuddin1140@gmail.com>
2025-05-28 11:17:41 +0600

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8 text-center">
+                <h1>Welcome</h1>
+                <h1 class="text-center">
+                    Custom Role & Permission System (Native)
+                </h1>
+            </div>
+        </div>
+    </div>
+@endsection
